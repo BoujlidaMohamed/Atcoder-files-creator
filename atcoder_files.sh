ef99c37c8c7746024e0b93951021232dc400964f
Ghassen <65939462+BoujlidaMohamed@users.noreply.github.com>
2021-08-21 19:40:40 +0900

    Update atcoder_files.sh

--- atcoder_files.sh
+++ atcoder_files.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-read -p " input competition number " COMP
+read -p " input competition number/name " COMP
 mkdir "$COMP"
+#Problem names, use spave to seperate between each problem name
 NAMES="A B C D E F G H"
 for NAME in $NAMES
   do
     touch "$COMP/$NAME.py"
+    #create code template, use \n for new line
     echo "n=int(input())\nl=[*map(int,input().split())]">>"$COMP/$NAME.py"
 done
  
